a81c66409f61aff40242dd01d21d2863004e09f2
Christian Tallner <christian.tallner@gmx.de>
2026-03-18 22:56:56 +0000

    Replace manual mkdir+cd+make pattern in install.sh with cmake -S/-B and ninja. Simplify uninstall.sh to use the CMake-generated ninja uninstall target. Add shebang and SPDX copyright to both scripts.

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
+#!/usr/bin/env bash
 # SPDX-FileCopyrightText: 2018 Michail Vourlakos <mvourlakos@gmail.com>
+# SPDX-FileCopyrightText: 2026 Christian Tallner <chrtall@gmx.de>
 # SPDX-License-Identifier: GPL-2.0-or-later
-*/
-if ! [ -a build ] ; then
-    mkdir build
-fi
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -Wnodev ..
-make -j$(nproc)
-sudo make install
+cmake -G Ninja -S . -B build -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -Wnodev
+ninja -C build
+sudo ninja -C build install
--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # SPDX-FileCopyrightText: 2018 Michail Vourlakos <mvourlakos@gmail.com>
+# SPDX-FileCopyrightText: 2026 Christian Tallner <chrtall@gmx.de>
 # SPDX-License-Identifier: GPL-2.0-or-later
-# Summary: Uninstallation script
-# This script was written and tested on openSuSe Leap 42.1
-if [ -f build/install_manifest.txt ]; then
-   echo "Uninstallation file exists..."
-   sudo xargs -d '\n' rm < build/install_manifest.txt
-else
-   echo "Uninstallation file does not exist."
-fi
+sudo ninja -C build uninstall
